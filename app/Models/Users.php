13df5b9a5249deb8db5e7b7104006ce52b81d835
juan-fonseca-cwbva_backbase <juan.fonseca@softtek.com>
2024-03-13 06:44:16 -0600

    Fix Notifiable error

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Users extends Authenticatable
@@ -18,7 +19,7 @@ class Users extends Authenticatable
         'email',
         'name',
         'picture',
-        'give_name',
+        'given_name',
         'family_name',
         'verified_email',
         'id'
